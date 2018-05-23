f014369cb52dd834e0a2f0d8ad34a46575b8b519
pravdomil <pravdomil.toman@gmail.com>
2018-05-24 00:49:37 +0200

    format

--- FixSketchTrackpadSpeed.sketchplugin/Contents/Sketch/script.js
+++ FixSketchTrackpadSpeed.sketchplugin/Contents/Sketch/script.js
@@ -1,26 +1,26 @@
-const frameworkName = "FixSketchTrackpadSpeed";
+const frameworkName = "FixSketchTrackpadSpeed"
 
 function onStart(context) {
-  const scriptPath = context.scriptPath;
-  const directory = scriptPath.stringByDeletingLastPathComponent();
+  const scriptPath = context.scriptPath
+  const directory = scriptPath.stringByDeletingLastPathComponent()
   const mocha = Mocha.sharedRuntime()
 
   const loaded = mocha.loadFrameworkWithName_inDirectory(frameworkName, directory)
   if (!loaded) {
-    log("FixSketchTrackpadSpeed loadFrameworkWithName failed");
-    return;
+    log("FixSketchTrackpadSpeed loadFrameworkWithName failed")
+    return
   }
 
-  const fix = NSClassFromString("FixSketchTrackpadSpeed");
+  const fix = NSClassFromString("FixSketchTrackpadSpeed")
   if (!fix) {
-    log("FixSketchTrackpadSpeed NSClassFromString failed");
-    return;
+    log("FixSketchTrackpadSpeed NSClassFromString failed")
+    return
   }
 
   if (!fix.fix()) {
-    log("FixSketchTrackpadSpeed fix failed");
-    return;
+    log("FixSketchTrackpadSpeed fix failed")
+    return
   }
 
-  log("FixSketchTrackpadSpeed fixed");
+  log("FixSketchTrackpadSpeed fixed")
 }
